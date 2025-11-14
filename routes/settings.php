90fa7d860991ce4745e153c6f38c934a7b6eea6b
sh3ki <shekaigarcia@gmail.com>
2025-11-14 17:05:27 +0800

    Fix route name conflicts - prefix settings routes with 'settings.'

--- routes/settings.php
+++ routes/settings.php
@@ -9,20 +9,20 @@ use Inertia\Inertia;
 Route::middleware('auth')->group(function () {
     Route::redirect('settings', '/settings/profile');
 
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('settings.profile.edit');
+    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('settings.profile.update');
+    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('settings.profile.destroy');
 
-    Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
+    Route::get('settings/password', [PasswordController::class, 'edit'])->name('settings.password.edit');
 
     Route::put('settings/password', [PasswordController::class, 'update'])
         ->middleware('throttle:6,1')
-        ->name('password.update');
+        ->name('settings.password.update');
 
     Route::get('settings/appearance', function () {
         return Inertia::render('settings/appearance');
-    })->name('appearance.edit');
+    })->name('settings.appearance.edit');
 
     Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
-        ->name('two-factor.show');
+        ->name('settings.two-factor.show');
 });
